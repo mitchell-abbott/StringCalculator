9a0d10e5c501794521a12f693fe9d2cd10fd3dad
Mitchell Abbott <mitchabbott7@hotmail.com>
2016-05-01 20:02:50 -0500

    Refactor first step

--- src/main/java/com/StringCalculator.java
+++ src/main/java/com/StringCalculator.java
@@ -5,16 +5,16 @@ import org.apache.commons.lang3.StringUtils;
 public class StringCalculator {
 
     int add(String numbers){
+        return StringUtils.isBlank(numbers) ? 0 : getAddedNumbers(numbers);
+    }
+
+    private int getAddedNumbers(String numbers) {
         int value;
-        if (StringUtils.isBlank(numbers)) {
-            value = 0;
+        String[] split = numbers.split(",");
+        if (split.length > 1) {
+            value = Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
         } else {
-            String[] split = numbers.split(",");
-            if (split.length > 1) {
-                value = Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
-            } else {
-                value = Integer.valueOf(split[0]);
-            }
+            value = Integer.valueOf(split[0]);
         }
         return value;
     }
